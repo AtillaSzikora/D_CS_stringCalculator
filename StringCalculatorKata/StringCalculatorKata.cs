e769323d0fabc910e74d185a2ab3a42d34a19d43
AtillaSzikora <szikora.atilla@gmail.com>
2016-04-01 10:21:20 +0200

    new tests and cases added

--- StringCalculatorKata/StringCalculatorKata.cs
+++ StringCalculatorKata/StringCalculatorKata.cs
@@ -3,31 +3,27 @@ using System.Linq;
 using static System.Int32;
 using static System.String;
 
-namespace StringCalculatorKata
-{
-    public class CalculatorKata
-    {
-        public static int Add(string textToAdd)
-        {
-            if (IsNullOrEmpty(textToAdd))
-            {
-                return 0;
-            }
-            textToAdd = textToAdd.Replace("\n", ",");
-            string[] numbers = textToAdd.Split(',');
+namespace StringCalculatorKata {
 
-            if (numbers.Contains(String.Empty))
-            {
-                throw new ArgumentException("Double following delimiter is not valid");
-            }
+    public class CalculatorKata {
+        private static char delimiter = ',';
 
+        public static int Add(string textToAdd) {
+            if (IsNullOrEmpty(textToAdd)) { return 0; }
+            if (textToAdd.StartsWith("//")) {
+                int firstNewLineIndex = textToAdd.IndexOf('\n');
+                string delimiterString = textToAdd.Substring(2, firstNewLineIndex - 2);
+                if (delimiterString.Length > 1) { throw new Exception("Custom delimiter length is invalid (max 1)"); }
+                delimiter = delimiterString[0]; }
+
+            textToAdd = textToAdd.Replace('\n', delimiter);
+            string[] numbers = textToAdd.Split('\n', delimiter);
             int sum = 0;
-            foreach (var number in numbers)
-            {
-                sum += Parse(number);
-            }
-            return sum;
-        }
+            foreach (var number in numbers) {
+                if (String.IsNullOrEmpty(number)) {
+                    throw new ArgumentException("Following chars each other are incorrect."); }
+                sum += Parse(number); }
+            return sum; }
 
         private static void Main(string[] args) {}
     }
--- StringCalculatorTests/StringCalculatorTests.cs
+++ StringCalculatorTests/StringCalculatorTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using StringCalculatorKata;
 
 namespace StringCalculatorTests
@@ -23,6 +24,24 @@ namespace StringCalculatorTests
         {
             int result = CalculatorKata.Add("");
             Assert.AreEqual(3, result);
+        }
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void AddCustomDelimiter_ThrowException()
+        {
+            int result = CalculatorKata.Add("//3\n3");
+            Assert.AreEqual(6, result);
+        }
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void AddCustomDelimiter_Return6()
+        {
+            int result = CalculatorKata.Add("//;\n1;2;3");
+            Assert.AreEqual(6, result);
+        }
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void AddCustomDelimiter_ThrowsException()
+        {
+            int result = CalculatorKata.Add("//;\n1-2-3");
+            Assert.AreEqual(6, result);
         }
     }
 }
